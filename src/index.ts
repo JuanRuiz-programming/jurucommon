f3b8a68e6f90f977f20aebc57c5dc00267bd6f46
JuanRuiz <juanscales@gmail.com>
2023-03-27 22:12:37 -0500

    Updated

--- src/index.ts
+++ src/index.ts
@@ -26,6 +26,8 @@ export * from "./events/auth-service/user-updated-event";
 export * from "./events/profile-service/profile-created-event";
 export * from "./events/profile-service/profile-demo-created-event";
 export * from "./events/profile-service/profile-updated-event";
+export * from "./events/profile-service/profile-deleted-event";
+export * from "./events/profile-service/profile-get-user-event";
 // Projects events
 export * from "./events/types/project-status";
 export * from "./events/project-service/project-cloned-event";
@@ -41,12 +43,26 @@ export * from "./events/project-service/project-updated-event";
 export * from "./events/project-service/project-winners-event";
 // Courses events
 export * from "./events/types/course-status";
+export * from "./events/course-service/course-assigned-event";
 export * from "./events/course-service/course-created-event";
+export * from "./events/course-service/course-deleted-event";
 export * from "./events/course-service/course-demo-created-event";
+export * from "./events/course-service/course-finished-event";
+export * from "./events/course-service/course-promoted-event";
+export * from "./events/course-service/course-published-event";
+export * from "./events/course-service/course-unpublished-event";
+export * from "./events/course-service/course-updated-event";
 // Activities events
 export * from "./events/types/activities-status";
 export * from "./events/types/activities-typologies";
+export * from "./events/activity-service/activity-assigned-event";
 export * from "./events/activity-service/activity-created-event";
+export * from "./events/activity-service/activity-deleted-event";
+export * from "./events/activity-service/activity-demo-created-event";
+export * from "./events/activity-service/activity-finished-event";
+export * from "./events/activity-service/activity-published-event";
+export * from "./events/activity-service/activity-unpublished-event";
+export * from "./events/activity-service/activity-updated-event";
 // A11y events
 export * from "./events/a11y-service/a11y-config-created-event";
 export * from "./events/a11y-service/a11y-config-updated-event";
