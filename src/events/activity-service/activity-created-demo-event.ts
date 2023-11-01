03283ac69523843b3b38031e3f825f0de6d380e5
JuanRuiz <juanscales@gmail.com>
2023-11-01 14:37:36 -0500

    Updated

--- src/events/activity-service/activity-created-demo-event.ts
+++ src/events/activity-service/activity-created-demo-event.ts
@@ -4,8 +4,7 @@ export interface ActivityDemoCreatedEvent {
   subject: Subjects.ActivityDemoCreated;
   data: {
     id: string;
-    author: string;
-    courses_related: string[];
+    owner: string;
     version: number;
   };
 }
