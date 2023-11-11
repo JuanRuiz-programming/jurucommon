0a041997089c5a55b303371abfdc0b4025752006
JuanRuiz <juanscales@gmail.com>
2023-11-11 16:25:48 -0500

    updated

--- src/events/course-service/course-updated-event.ts
+++ src/events/course-service/course-updated-event.ts
@@ -5,8 +5,32 @@ export interface CourseUpdatedEvent {
   subject: Subjects.CourseUpdated;
   data: {
     id: string;
-    author: string[];
     owner: string;
-    version: number;
+    author: string[];
+    type: string;
+    scope: string;
+    level: string;
+    size: string;
+    complexity: string;
+    title: string;
+    desc: string;
+    main_topic: string;
+    secundary_topic: string[];
+    learning_goals: string[];
+    sections: string[];
+    lessons: string[];
+    schedule: string;
+    area: string;
+    docs: string[];
+    tools: string[];
+    promo_media: string[];
+    status: string;
+    students: string[];
+    activities: string[];
+    assessments: string[];
+    teams: string[];
+    projects_related: string[];
+    courses_related: string[];
+    tutoring: string[];
   };
 }
