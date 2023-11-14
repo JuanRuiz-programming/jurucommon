6f55364599401b1cf8bcae87cbc2cf97349ece26
JuanRuiz <juanscales@gmail.com>
2023-11-14 11:57:36 -0500

    updated

--- src/events/activity-service/activity-updated-event.ts
+++ src/events/activity-service/activity-updated-event.ts
@@ -6,5 +6,23 @@ export interface ActivityUpdatedEvent {
     id: string;
     owner: string;
     version: number;
+    author: string;
+    type: string;
+    typology: string;
+    scope: string;
+    level: string;
+    complexity: string;
+    area: string;
+    title: string;
+    prompt: string;
+    content: string;
+    media: string[];
+    docs: string[];
+    tools: string[];
+    status: string;
+    feedback: string;
+    gradable: boolean;
+    courses_related: string[]
+    timer: string;
   };
 }
